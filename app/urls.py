86d442119547329693abb727456e67ef98f09dc3
Sergei Ershov <codemathphyinformation@gmail.com>
2024-05-15 00:41:47 +0400

    Added and move log&reg

--- app/urls.py
+++ app/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 from main import views
 
@@ -23,4 +23,5 @@ from main import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.index, name='index'),
+    path('user/', include('users.urls', namespace='user')),
 ]
--- users/views.py
+++ users/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
 
 # Create your views here.
+def login(request):
+    context={
+        'title':'Home - Авторизация'
+    }
+    return render(request, 'login.html', context)
+
+
+def registration(request):
+    context={
+        'title':'Home - Регистрация'
+    }
+    return render(request, 'registration.html', context)
+
+
+def logout(request):
+    ...
